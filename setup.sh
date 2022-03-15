13ea24165e50173c5e35fc5e20f2506f027e3314
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2022-03-15 08:54:57 -0500

    Added unattended flag to zsh install

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ $HOME/.dotfiles/migrate.sh
 #Install Oh My Zsh and plugins
 
 if [[ ! -d $HOME/.oh-my-zsh ]]; then
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
     git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
