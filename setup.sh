7055036fc1df83d79dcdea295ef8169aa8d6f44d
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2022-03-15 10:00:35 -0400

    Added ZSH_CUSTOM definition

--- setup.sh
+++ setup.sh
@@ -13,6 +13,8 @@ $HOME/.dotfiles/migrate.sh
 if [[ ! -d $HOME/.oh-my-zsh ]]; then
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" "" --unattended
 
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
+
     git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
