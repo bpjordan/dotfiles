d1a7c4c2f7ca6cc0a0e9c5df23d48d1d1ece5b65
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2022-09-30 09:51:48 -0500

    Fixed failure test in setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 #Install all dependecies
 
 sudo apt install curl wget vim zsh -y
-if [[ $? -neq 0 ]]; then echo "Required dependencies failed to install, aborting" && return 1; fi
+if [[ $? -ne 0 ]]; then echo "Required dependencies failed to install, aborting" && return 1; fi
 
 sudo apt install neofetch -y
 
