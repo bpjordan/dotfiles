3060e1ecec5b503862256abf3e53d65646a90022
Bronson Jordan <80419011+bpjordan@users.noreply.github.com>
2023-06-16 10:41:00 -0500

    nvim: add grep search keybind back

--- .config/nvim/lua/my/keymaps.lua
+++ .config/nvim/lua/my/keymaps.lua
@@ -56,6 +56,7 @@ vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { des
 vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').git_files, { desc = '[S]earch in [G]it repo' })
+vim.keymap.set('n', '<leader>sG', require('telescope.builtin').live_grep, { desc = '[S]earch with [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
 
 -- [[ Configure Treesitter ]]
