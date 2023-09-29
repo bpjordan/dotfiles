c6c5e4623d2231cefb4402058cdead9bcf75d678
Bronson Jordan <bronson.jordan@praeses.com>
2023-09-29 09:08:11 -0500

    feat(nvim): enable stylua and trim_whitespace formatters

--- .config/nvim/lua/my/plugins/lsp.lua
+++ .config/nvim/lua/my/plugins/lsp.lua
@@ -48,12 +48,12 @@ return { -- LSP Configuration & Plugins
         rust = { 'rustfmt' },
         javascript = { { 'eslint_d', 'prettierd', 'prettier' } },
         json = { { 'jq', 'prettierd', 'prettier' } },
+        lua = { 'stylua' },
+        ['_'] = { 'trim_whitespace' },
       },
       notify_on_error = true,
       format_on_save = function(bufnr)
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
         return {
           timeout_ms = 500,
           lsp_fallback = true,
